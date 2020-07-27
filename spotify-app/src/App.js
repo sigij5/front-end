447a42dfc629d58376a8c4e2c2e7c9b672b8f737
Sigi Jaeckel <sigij55@gmail.com>
2020-07-27 12:45:58 -0400

    built react app framework

---
+++ spotify-app/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Route, NavLink, Switch } from 'react-router-dom'
+
+
+import Login from './components/Login'
+import Signup from './components/Signup'
+import './App.css';
+import PrivateRoute from './components/PrivateRoute';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='App-header'>
+        <h1>Spotify App</h1>
+        <NavLink className='link' to='login'>Login</NavLink>
+        <NavLink className='link' to='signup'>Sign Up</NavLink>
+      </header>
+      <Switch>
+        <Route path='/login' component={Login} />
+        <Route path='/signup' component={Signup} />
+        <PrivateRoute path='/profile' />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
